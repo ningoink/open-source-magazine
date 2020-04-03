84ee3bc36b5a2c7da2fe43f0adfff9dd63938048
hylerrix <hylerrix@gmail.com>
2020-04-03 10:54:20 +0800

    docs: rename app short name into osm
    Signed-off-by: hylerrix <hylerrix@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   })
 
-  it(`should have as title 'client'`, () => {
+  it(`should have as title 'osm'`, () => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.componentInstance
-    expect(app.title).toEqual('client')
+    expect(app.title).toEqual('osm')
   })
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
     const compiled = fixture.nativeElement
-    expect(compiled.querySelector('.content span').textContent).toContain('client app is running!')
+    expect(compiled.querySelector('.content span').textContent).toContain('osm app is running!')
   })
 })
