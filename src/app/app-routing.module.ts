ba57d71b575c328e7bfff17b5a46658d6e805024
hylerrix <hylerrix@gmail.com>
2020-04-27 15:31:40 +0800

    refactor: rename presentation floder into pages
    Signed-off-by: hylerrix <hylerrix@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
-import { OsmBrowserComponent } from './presentation/osm-browser/osm-browser.component'
-import { OsmExplorerComponent } from './presentation/osm-explorer/osm-explorer.component'
+import { OsmBrowserComponent } from './pages/osm-browser/osm-browser.component'
+import { OsmExplorerComponent } from './pages/osm-explorer/osm-explorer.component'
 
 const routes: Routes = [
   { path: '', component: OsmBrowserComponent },
