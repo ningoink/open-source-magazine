0ccbb09c3f771ef78a4d1287ae4cbf52e4e71060
hylerrix <hylerrix@gmail.com>
2020-04-27 17:24:40 +0800

    style: import sidenav comp from material.angular.io
    Signed-off-by: hylerrix <hylerrix@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { OsmBrowserComponent } from './pages/osm-browser/osm-browser.component'
-import { OsmExplorerComponent } from './pages/osm-explorer/osm-explorer.component'
+// import { OsmExplorerComponent } from './pages/osm-explorer/osm-explorer.component'
 
 const routes: Routes = [
   { path: '', component: OsmBrowserComponent },
-  { path: 'explore', component: OsmExplorerComponent },
+  // { path: 'explore', component: OsmExplorerComponent },
 ]
 
 @NgModule({
